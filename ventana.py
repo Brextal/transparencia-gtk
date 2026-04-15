03a04900f70854250beed49919f21f681652e488
Brextal <xcorpsexxgrinderx@gmail.com>
2026-04-15 18:53:33 -0400

    Remove unnecessary blank line in ventana.py

--- ventana.py
+++ ventana.py
@@ -7,8 +7,7 @@ import sys
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk, GLib
 
-LOG_LEVEL = "INFO"  # DEBUG, INFO, WARNING, ERROR
-
+LOG_LEVEL = "INFO"  
 
 def log(level, message):
     if level == "DEBUG" and LOG_LEVEL != "DEBUG":
